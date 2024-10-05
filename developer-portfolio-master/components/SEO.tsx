811e70701a9e26027a923b0bd053ab928088fc52
Saurabh Mishra <saurabhmishra1464@gmail.com>
2024-10-06 03:31:22 +0530

    updated

--- developer-portfolio-master/components/SEO.tsx
+++ developer-portfolio-master/components/SEO.tsx
@@ -5,7 +5,7 @@ import { seoData } from "../portfolio";
 function SEO() {
   return (
     <Head>
-      {/* <title>{seoData.title}</title> */}
+      <title>{seoData.title}</title>
       <meta name="title" content={seoData.title} />
       <meta name="author" content={seoData.author} />
       <meta name="description" content={seoData.description} />
